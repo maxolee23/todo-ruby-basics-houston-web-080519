25f51b7b0cfbbcab40d8e061c2a1c1a53161d520
Max Lee <maxolee23@gmail.com>
2019-08-09 15:51:25 +0000

    Automatically backed up by Learn

--- lib/ruby_basics.rb
+++ lib/ruby_basics.rb
@@ -3,25 +3,25 @@ def division(num1, num2)
 end
 
 def assign_variable(value)
-
+  assign_variable = value
 end
 
-def argue
-
+def argue(argument)
+  return argument
 end
 
-def greeting
-
+def greeting(greeting, name)
+  
 end
 
 def return_a_value
-
+  puts "Nice"
 end
 
 def last_evaluated_value
-
+  puts "expert"
 end
 
-def pizza_party
-
+def pizza_party(arg = "cheese")
+  puts arg 
 end
